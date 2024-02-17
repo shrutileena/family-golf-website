e65f77690106a18738d3bf61ed35801ec75a1745
shruti agarwal <shrutileena@gmail.com>
2024-02-17 18:15:41 +0530

    additions

--- family-golf/script.js
+++ family-golf/script.js
@@ -1,3 +1,13 @@
+var cursor = document.querySelector("#cursor")
+var cursorBlur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove", function (dets) {
+        cursor.style.left = dets.x + "px"
+        cursor.style.top = dets.y + "px"
+        cursorBlur.style.left = dets.x - "150" + "px"
+        cursorBlur.style.top = dets.y - "150" + "px"
+    }
+)
+
 gsap.to("#nav", {
     backgroundColor : "#95C11E",
     height : "100px",
@@ -10,5 +20,17 @@ gsap.to("#nav", {
         start : "top -10%",
         end : "top -11%",
         scrub : 1
+    }
+})
+
+gsap.to("#main", {
+    backgroundColor : "#000",
+    scrollTrigger : {
+        trigger : "#main",
+        scroller : "body",
+        // markers : true,
+        start : "top -25%",
+        end : "top -70%",
+        scrub : 2
     }
 })
